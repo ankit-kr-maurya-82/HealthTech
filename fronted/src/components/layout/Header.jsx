d3afe3a9601125ba3a8a361082d13a055d8be379
AK Maurya <143257696+ankit-kr-maurya-82@users.noreply.github.com>
2026-02-10 21:04:39 +0530

    fix css import

--- fronted/src/components/layout/Header.jsx
+++ fronted/src/components/layout/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
-import "./css/Footer.css"
+import "./css/Header.css"
 
 const Header = ({ isLoggedIn }) => {
   return (

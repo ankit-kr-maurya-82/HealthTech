2f5d88c43750fffdcdcb6c35370a786e786b13db
AK Maurya <143257696+ankit-kr-maurya-82@users.noreply.github.com>
2026-02-11 16:03:48 +0530

    bg fix mongodb

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,15 +1,9 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-import { app } from "./app.js";
 
-dotenv.config();
+dotenv.config({
+    path: './env'
+});
 
 connectDB()
-.then(() => {
-    app.listen(process.env.PORT || 8000, () => {
-        console.log(`🚀 Server running on port ${process.env.PORT}`);
-    });
-})
-.catch((err) => {
-    console.log("❌ Mongo DB connection error !!!", err);
-});
+

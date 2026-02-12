fe04b191c78d04d996479a7b2b94e0d374de2407
AK Maurya <ankitkrmaurya82@gmail.com>
2026-02-12 23:13:05 +0530

    add css of Doctor and patient

--- fronted/src/pages/auth/DoctorRegister.jsx
+++ fronted/src/pages/auth/DoctorRegister.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import "./css/DoctorRegister.css"
 
 const DoctorRegister = () => {
   const navigate = useNavigate();
--- fronted/src/pages/auth/PatientLogin.jsx
+++ fronted/src/pages/auth/PatientLogin.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import "./css/PatientLogin.css"
 
 const PatientLogin = () => {
   const navigate = useNavigate();

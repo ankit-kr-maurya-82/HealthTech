b8ec315e7c014e548d89fb91a3258feb22376230
AK Maurya <ankitkrmaurya82@gmail.com>
2026-02-13 09:30:27 +0530

    add UserContextProvider

--- fronted/src/Layout.jsx
+++ fronted/src/Layout.jsx
@@ -1,16 +1,26 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
 import Header from './components/layout/Header'
 import Footer from './components/layout/Footer'
 import Sidebar from './components/layout/Sidebar'
+import UserContextProvider from './assets/context/UserContext'
 
 const Layout = () => {
+    const location = useLocation();
+     const hideHeader =
+    location.pathname.startsWith("/login") ||
+    location.pathname.startsWith("/register");
+
   return (
     <>
-    {/* <Sidebar/> */}
-        <Header/>
-        <Outlet/>
-        <Footer/>
+    <UserContextProvider>
+        {!hideHeader && <Header />}
+        {/* {!hideHeader && <Sidebar />} */}
+
+        <main>
+          <Outlet />
+        </main>
+      </UserContextProvider>
     </>
   )
 }

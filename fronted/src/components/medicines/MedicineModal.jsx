44145e8ea52d3c9cfd767461d25e6bc3052baf40
AK Maurya <ankitkrmaurya82@gmail.com>
2026-02-14 23:29:34 +0530

    bg fix but mongodb atlas not connected

---
+++ fronted/src/components/medicines/MedicineModal.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MedicineModal = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default MedicineModal
---
+++ fronted/src/components/medicines/MedicineSearch.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MedicineSearch = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default MedicineSearch

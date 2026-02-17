5a8d0b76efcdc729d4bfd0a6d1a8131e45345519
AK Maurya <ankitkrmaurya82@gmail.com>
2026-02-17 21:00:59 +0530

    add photo of developer

--- fronted/src/pages/public/Team.jsx
+++ fronted/src/pages/public/Team.jsx
@@ -1,6 +1,27 @@
 import React from "react";
 import members from "./teamData.json";
 import "./css/Team.css";
+import userAvatar from "../../assets/user.png";
+
+const assetImages = import.meta.glob("../../assets/*", {
+  eager: true,
+  import: "default",
+});
+
+const findAssetByFileName = (imgPath) => {
+  const normalized = imgPath.replace(/\\/g, "/");
+  const fileName = normalized.split("/").pop();
+  const matchKey = Object.keys(assetImages).find((key) =>
+    key.endsWith(`/${fileName}`)
+  );
+  return matchKey ? assetImages[matchKey] : null;
+};
+
+const getMemberImage = (img) => {
+  if (!img) return userAvatar;
+  if (img.startsWith("http://") || img.startsWith("https://")) return img;
+  return findAssetByFileName(img) || userAvatar;
+};
 
 const Team = () => {
   return (
@@ -10,7 +31,7 @@ const Team = () => {
       <div className="team-container">
         {members.map((member, index) => (
           <div className="team-card" key={index}>
-            <img src={member.img} alt={member.name} />
+            <img src={getMemberImage(member.img)} alt={member.name} />
             <h3>{member.name}</h3>
             <p>{member.role}</p>
           </div>
